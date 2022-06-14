f0a33f86ef04646b1fcccf15eb29acc378af2dd4
empobla-redux <A01027265@itesm.mx>
2022-06-13 21:38:28 -0500

    Fixed typos

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TC/IP Congestion Control: Scaleable TCP
+# TC/IP Congestion Control: Scalable TCP
 
 ## Authors
 - Emilio Popovits Blake
@@ -8,9 +8,9 @@
 - Rodrigo Benavente García
 
 ## Description
-`Scaleable_TCP.py` simulates the connection phase of TCP (through a three-way handshake) and the transfer phase of TCP with congestion, using the implemented Scaleable TCP TC/IP congestion algorithm.
+`Scalable_TCP.py` simulates the connection phase of TCP (through a three-way handshake) and the transfer phase of TCP with congestion, using the implemented Scalable TCP TC/IP congestion algorithm.
 
-**Note**: `Scaleable_TCP.py` only simulates the Scaleable TCP TC/IP congestion algorithm, meaning that, in cases where it's Decrease Procedures would decrease the `cwnd` and `sst` values to values where TCP would instead use TCP Congestion Avoidance, it does not perform the Decrease Procedure so only Scaleable TCP is simulated (as standard TCP Congestion is not implemented).
+**Note**: `Scalable_TCP.py` only simulates the Scalable TCP TC/IP congestion algorithm, meaning that, in cases where it's Decrease Procedures would decrease the `cwnd` and `sst` values to values where TCP would instead use TCP Congestion Avoidance, it does not perform the Decrease Procedure so only Scalable TCP is simulated (as standard TCP Congestion is not implemented).
 
 ## Requirements
 - Python3
@@ -20,10 +20,10 @@ In order to run this code Python3 must be installed.
 
 To run this code, run the following command on console:
 ```sh
-python Scaleable_TCP.py
+python Scalable_TCP.py
 ```
 
-If you want to see more clearly Scaleable TCP's Decrease Procedures, in `Scaleable_TCP.py` you can comment the following line in the `main` function (line 155):
+If you want to see more clearly Scalable TCP's Decrease Procedures, in `Scalable_TCP.py` you can comment the following line in the `main` function (line 155):
 ```py
 runSimulation()
 ```
@@ -31,4 +31,4 @@ and un-comment the following line (line 159):
 ```py
 # runSimulation(packets = 100, a_s = 10, b_s = 0.875)
 ```
-which increases Scaleable TCP's additive increase from `0.01` (default) to `10`, making it so that the Decrease Procedure can run and decrease `cwnd` and `sst` without running into TCP Congestion Avoidance.
+which increases Scalable TCP's additive increase from `0.01` (default) to `10`, making it so that the Decrease Procedure can run and decrease `cwnd` and `sst` without running into TCP Congestion Avoidance.
--- Scalable_TCP.py
+++ Scalable_TCP.py
@@ -63,8 +63,8 @@ def runSimulation(packets = 100, additive_increase = 0.01, multiplicative_decrea
     Runs TCP simulation
     
     :param int packets: number of packets (default 100)
-    :param float additive_increase: additive increase to use for Scaleable TCP (default 0.01)
-    :param float multiplicative_decrease: multiplicative decrease to use for Scaleable TCP. Must be high above 0.5 and below 1 (default 0.875)
+    :param float additive_increase: additive increase to use for Scalable TCP (default 0.01)
+    :param float multiplicative_decrease: multiplicative decrease to use for Scalable TCP. Must be high above 0.5 and below 1 (default 0.875)
 
     :return void
     '''
@@ -80,32 +80,32 @@ def runSimulation(packets = 100, additive_increase = 0.01, multiplicative_decrea
     '''
     
     # Assume that cwnd (congestion window) is greater than the low-window threshold for 
-    # Scaleable TCP, to simulate Scaleable TCP procedures
+    # Scalable TCP, to simulate Scalable TCP procedures
     cwnd_int = 20   # Initial congestion window in packets, set to 20 for simulation purposes
     cwnd = cwnd_int # Congestion window in packets
     sst_int = 25    # Slow start threshold to terminate initial slow start (in packets)
     sst = sst_int   # Current slow start threshold in packets
 
-    # Initialize Scaleable TCP symbols
-    a_s = additive_increase        # Increase applied to cwnd by Scaleable TCP on each ACK
-    b_s = multiplicative_decrease  # Percentage residual congestion window applied by Scaleable TCP on each loss
-    lw_s = 16                      # Low-window threshold for applying Scaleable TCP procedures (in packets)
+    # Initialize Scalable TCP symbols
+    a_s = additive_increase        # Increase applied to cwnd by Scalable TCP on each ACK
+    b_s = multiplicative_decrease  # Percentage residual congestion window applied by Scalable TCP on each loss
+    lw_s = 16                      # Low-window threshold for applying Scalable TCP procedures (in packets)
 
     # While there are still packets to be sent
     while (packets > 0):
         # A timeout can occur, so simulate it with a 5% chance
         timeout = True if randint(1, 20) == 1 else False
 
-        # Select wether to use TCP Congestion Avoidance or Scaleable TCP Congestion Avoidance
-        # Will always select Scaleable TCP Congestion Avoidance for simulation purposes
-        use_scaleable_tcp = cwnd > lw_s
+        # Select wether to use TCP Congestion Avoidance or Scalable TCP Congestion Avoidance
+        # Will always select Scalable TCP Congestion Avoidance for simulation purposes
+        use_Scalable_tcp = cwnd > lw_s
 
-        if use_scaleable_tcp:
-            print(f'\n[SOURCE] Using Scaleable TCP for packets {packets}-{(packets - int(cwnd)) if packets >= int(cwnd) else 0} with a low-window threshold of {lw_s}')
+        if use_Scalable_tcp:
+            print(f'\n[SOURCE] Using Scalable TCP for packets {packets}-{(packets - int(cwnd)) if packets >= int(cwnd) else 0} with a low-window threshold of {lw_s}')
 
             # Timeout procedure
             # Set the slow start threshold to the reduced cwnd and reset the cwnd to its 
-            # initial value, meaning that Scaleable TCP will reenter slow start until 
+            # initial value, meaning that Scalable TCP will reenter slow start until 
             # the cwnd passes the sst and then return to congestion avoidance
             if timeout:
                 print(f'[SOURCE] Timed Out: Setting Slow Start Threshold to {max(cwnd * b_s, cwnd_int)}')
@@ -119,7 +119,7 @@ def runSimulation(packets = 100, additive_increase = 0.01, multiplicative_decrea
             ack_received = True if randint(1, 10) <= 2 else False
             
             # Increase procedure
-            # If no packets were lost, increase cwnd by Scaleable TCP's fixed value
+            # If no packets were lost, increase cwnd by Scalable TCP's fixed value
             if ack_received: 
                 print(f'[SOURCE] Successfully sent packets {packets}-{(packets - int(cwnd)) if packets >= int(cwnd) else 0}')
                 print(f'[SOURCE] Increasing congestion window threshold from {round(cwnd, 2)} to {round(cwnd + a_s, 2)}')
@@ -130,17 +130,17 @@ def runSimulation(packets = 100, additive_increase = 0.01, multiplicative_decrea
                 continue
 
             # Decrease procedure
-            # If there was a loss notification, reduce the cwnd by Scaleable TCP's percentage 
+            # If there was a loss notification, reduce the cwnd by Scalable TCP's percentage 
             # residual congestion. Also, set the slow start threshold to the new, lower cwnd 
             # to ensure the flow remains in congestion avoidance
             # 
-            # In order to simulate ONLY Scaleable TCP, cwnd must not go under the low-window 
+            # In order to simulate ONLY Scalable TCP, cwnd must not go under the low-window 
             # threshold.
             if not ack_received: 
                 print(f'[SOURCE] Packet loss recieved on packets {packets}-{(packets - int(cwnd)) if packets >= int(cwnd) else 0}')
                 
                 if cwnd * b_s < lw_s:
-                    print(f'[SIMULATION] Decrease procedure not executed in order to ONLY simulate Scaleable TCP')
+                    print(f'[SIMULATION] Decrease procedure not executed in order to ONLY simulate Scalable TCP')
                     print(f'[SIMULATION] If decrease procedure had been executed, TCP Congestion Avoidance would have run instead')
                     continue
 
@@ -151,9 +151,9 @@ def runSimulation(packets = 100, additive_increase = 0.01, multiplicative_decrea
                 continue
                     
 if __name__ == '__main__':  
-    # Run Scaleable TCP simulation with default values (from the PDF)
+    # Run Scalable TCP simulation with default values (from the PDF)
     runSimulation()
     
-    # In order to make Scaleable TCP's decrease procedure noticeable, run 
+    # In order to make Scalable TCP's decrease procedure noticeable, run 
     # the simulation with the following values:
     # runSimulation(packets = 100, a_s = 10, b_s = 0.875)  
